eec87ba95ae9d7592780513e1e41ae5b86c0c48c
Marto_0 <martvanenck1@gmail.com>
2019-05-17 18:30:13 +0200

    tag command

---
+++ commands/tag.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js')
+const database = require('../scripts/database.js')
+const utils = require('../scripts/utils.js')
+
+module.exports = async (message) => {
+
+  let args = message.content.split(' ')
+  let tag = args[1]
+
+  database.updateClan(message.guild.id, {tag: tag})
+
+  message.channel.send('Succesfully edited clan tag')
+}
