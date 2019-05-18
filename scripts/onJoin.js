561c4f3d7601ab4b43636480298102955a0e4338
Marto_0 <martvanenck1@gmail.com>
2019-05-18 17:43:03 +0200

    fix

--- scripts/onJoin.js
+++ scripts/onJoin.js
@@ -12,7 +12,7 @@ module.exports = async (guild) => {
 
 let onJoinText = `
 Thanks for inviting me to your HelmetRoyale clan!
-Type \`${prefix}help\` For more commands of the bot.
+Type \`${p}help\` For more commands of the bot.
 If you want to register this server as a clan on my site, use \`${p}registerclan\`
 If you have any questions, Feel free to DM the developer (Marto_0#1978)
 `
