1556d4cbdd9507da80d4e1ae91104efdf30cb495
Marto_0 <martvanenck1@gmail.com>
2019-05-21 12:46:29 +0200

    fixed bot permissions

--- commands/invite.js
+++ commands/invite.js
@@ -2,5 +2,5 @@ const Discord = require('discord.js')
 const database = require('../scripts/database.js')
 
 module.exports = async (message) => {
-  message.channel.send('Invite this bot to a server using this link: \nhttps://discordapp.com/oauth2/authorize?client_id=578905640181825556&permissions=67585&scope=bot')
+  message.channel.send(`Invite this bot to a server using this link: \nhttps://discordapp.com/oauth2/authorize?client_id=578905640181825556&permissions=${process.env.botperms}&scope=bot`)
 }
