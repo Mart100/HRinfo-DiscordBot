fee0564eaa8037c4956dca11ce79ae9ff737bcde
Marto_0 <martvanenck1@gmail.com>
2019-05-28 11:30:53 +0200

    updated connectgame

--- commands/connectgame.js
+++ commands/connectgame.js
@@ -5,10 +5,12 @@ module.exports = async (message) => {
 
   let args = message.content.split(' ')
   let p = process.env.prefix
-  let userID = args[1]
-  let stats = await HRapi.getUserStats(userID)
+  let gameID = args[1]
+  let stats = await HRapi.getUserStats(gameID)
   
   if(stats == undefined) return message.channel.send(`ID undefined, Example: \`${p}connectgame Google_115855350808462018005\``)
+
+  database.updatePlayer(message.author.id, 'gameID', gameID)
 
   message.channel.send(`Successfully connected as \`${stats.name}\``)
 }
